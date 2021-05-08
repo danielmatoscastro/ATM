b0c5f155324cdc5c3804f4df983bb925cc4b90c2
Daniel Matos de Castro <daniel.matos.castro@gmail.com>
2021-05-08 12:25:38 -0300

    turning off menu sorting.

--- atm-proto/src/contexts/ShouldSortByFreqProvider/index.js
+++ atm-proto/src/contexts/ShouldSortByFreqProvider/index.js
@@ -3,18 +3,11 @@ import PropTypes from 'prop-types';
 import { v4 as uuidv4 } from 'uuid';
 import { ShouldSortByFreqContext } from './ShouldSortByFreqContext';
 
-function getRandomBool() {
-  const min = 0;
-  const max = 2;
-  return (Math.floor(Math.random() * (max - min)) + min) % 2 === 0;
-}
-
 export const ShouldSortByFreqProvider = ({ children }) => {
-  const [shouldSortByFreq, setShouldSortByFreq] = useState(true);
+  const [shouldSortByFreq] = useState(false);
   const [id, setId] = useState('');
 
   useEffect(() => {
-    setShouldSortByFreq(getRandomBool());
     setId(uuidv4());
   }, []);
 
